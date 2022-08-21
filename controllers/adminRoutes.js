9e4caca37cb67c5e0c3c46d91ddb67476248f7ff
Nate <itsnzte@gmail.com>
2022-08-20 22:22:52 -0700

    added admin file route with starter code, and required it in index of controllers folder

---
+++ controllers/adminRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  try {
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const adminRoutes = require('./adminRoutes');
 
 router.use('/', homeRoutes);
+router.use('/admin', adminRoutes);
 
 module.exports = router;
