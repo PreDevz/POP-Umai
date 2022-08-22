8b9b808a9425ca3ff22e224b108bfd27b17e3997
aq032 <anthonyq032@gmail.com>
2022-08-22 12:42:29 -0700

    Created db model for upcoming and past events

--- models/event.js
+++ models/event.js
@@ -1,1 +1,50 @@
-// Model for events to the website will go here
+// Model for events to the website will go hereconst { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize/types');
+const sequelize = require('../config/connection');
+
+class Event extends Model {}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING(1000),
+    },
+    event_date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    event_time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    venue: {
+      type: DataTypes.STRING,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_upcoming: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'event',
+  }
+);
+
+module.exports = Event;
