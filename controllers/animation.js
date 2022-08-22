a05ab6b965b8c40c0b1db1687f23168e1157aa44
Cesar's PC <cesar261110@gmaill.com>
2022-08-22 15:21:17 -0700

    alignment fixes, testing image on events page, looking for fix on movile for vanta

--- controllers/animation.js
+++ controllers/animation.js
@@ -1,30 +1,30 @@
 
-window.addEventListener('DOMContentLoaded', () => {
-    VANTA.BIRDS({
-        el: "#vanta",
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-        color1: 0xef00ff,
-        color2: 0xffe100,
-        colorMode: "lerpGradient",
-        birdSize: 2.10,
-        separation: 51.00,
-        quantity: 2.00,
-        backgroundAlpha: 0.00
-    })
+// window.addEventListener('DOMContentLoaded', () => {
+//     VANTA.BIRDS({
+//         el: "#vanta",
+//         mouseControls: true,
+//         touchControls: true,
+//         gyroControls: false,
+//         minHeight: 200.00,
+//         minWidth: 200.00,
+//         scale: 1.00,
+//         scaleMobile: 1.00,
+//         color1: 0xef00ff,
+//         color2: 0xffe100,
+//         colorMode: "lerpGradient",
+//         birdSize: 2.10,
+//         separation: 51.00,
+//         quantity: 2.00,
+//         backgroundAlpha: 0.00
+//     })
 
-    setTimeout(() => {
-        const vanta = document.querySelector('#vanta')
+//     setTimeout(() => {
+//         const vanta = document.querySelector('#vanta')
 
-        vanta.style.opacity = 0
-    }, 7000)
+//         vanta.style.opacity = 0
+//     }, 7000)
 
-})
+// })
 
 
 
