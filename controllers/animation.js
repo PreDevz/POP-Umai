1fa85c6ead2aaa25e49e0c51ee4f056ef666ab16
Cesar's PC <cesar261110@gmaill.com>
2022-08-21 21:50:54 -0700

    Fix to when hovering over links, svg for second section, timeout on vanta.js to not take up bandwidth, still need fix to mobile using vanta.js

--- controllers/animation.js
+++ controllers/animation.js
@@ -1,17 +1,32 @@
-VANTA.BIRDS({
-    el: "#vanta",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color1: 0xef00ff,
-    color2: 0xffe100,
-    colorMode: "lerpGradient",
-    birdSize: 2.10,
-    separation: 51.00,
-    quantity: 2.00,
-    backgroundAlpha: 0.00
+
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color1: 0xef00ff,
+        color2: 0xffe100,
+        colorMode: "lerpGradient",
+        birdSize: 2.10,
+        separation: 51.00,
+        quantity: 2.00,
+        backgroundAlpha: 0.00
+    })
+
+    setTimeout(() => {
+        const vanta = document.querySelector('#vanta')
+
+        vanta.style.opacity = 0
+    }, 7000)
+
 })
+
+
+
+
+
