4c28015ee4c2725773cc3a741295475d07b51d2e
aq032 <anthonyq032@gmail.com>
2022-08-22 12:41:17 -0700

    Created db model for user reviews

--- models/review.js
+++ models/review.js
@@ -1,1 +1,40 @@
 // Model for reviews to the website will go here
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(1000),
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
