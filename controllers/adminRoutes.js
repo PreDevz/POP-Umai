ee0b6473c58710f2820735745e91e9a4828cde4c
Nate <itsnzte@gmail.com>
2022-08-25 12:27:19 -0700

    added admin model into admin routes, and added logout POST request to destory session

--- controllers/adminRoutes.js
+++ controllers/adminRoutes.js
@@ -1,8 +1,9 @@
 // Administration Page
 
 const auth = require('../utils/auth')
-
 const router = require('express').Router();
+const { Admin } = require('../models');
+const session = require('express-session');
 
 // admin login 
 router.get('/', async (req, res) => {
@@ -20,33 +21,33 @@ router.get('/', async (req, res) => {
 // admin login check 
 router.post('/login', async (req, res) => {
   
-  // try {
-  //   // check the database for user that matches email 
-  //   const adminData = await admin.findOne({
-  //     where:
-  //       { email: req.body.email }
-  //   });
+  try {
+    // check the database for user that matches email 
+    const adminData = await Admin.findOne({
+      where:
+        { email: req.body.email }
+    });
 
-  //   // check database to see if passwords match
-  //   const isValidPassword = await adminData.checkPassword(req.body.password);
+    // check database to see if passwords match
+    const isValidPassword = await adminData.checkPassword(req.body.password);
 
-  //   if (!isValidPassword) {
-  //     res.status(400).json({ message: 'Wrong Email or Password' });
-  //     return;
-  //   }
+    if (!isValidPassword) {
+      res.status(400).json({ message: 'Wrong Email or Password' });
+      return;
+    }
 
-  //   // Admin's Session 
-  //   req.session.save(() => {
-  //     req.session.user_id = adminData.id;
-  //     req.session.logged_in = true;
+    // Admin's Session 
+    req.session.save(() => {
+      req.session.user_id = adminData.id;
+      req.session.logged_in = true;
       
-  //     res.json({ user: adminData, message: 'logged in!' });
-  //   });
-  // } catch (err) {
+      res.json({ user: adminData, message: 'logged in!' });
+    });
+  } catch (err) {
 
-  //   // catching server errors 
-  //   res.status(500).json({ message: "Server Error", error: err })
-  // }
+    // catching server errors 
+    res.status(500).json({ message: "Server Error", error: err })
+  }
 
 })
 
@@ -60,6 +61,21 @@ router.get('/dashboard', auth, async (req, res) => {
 
     // catching server errors 
     res.status(500).json(err);
+  }
+});
+
+// Logout 
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+
+    // if there is a session, destory it 
+    req.session.destroy(() => {
+      res.status(200).end();
+    });
+  } else {
+
+    // if there's no session, send back 404 
+    res.status(404).end();
   }
 });
 
