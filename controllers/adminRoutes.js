4d91c1d9299472b23bd98b81e125c5dfee952791
Nate <itsnzte@gmail.com>
2022-08-25 11:57:14 -0700

    added checking for admin's email, password, and session

--- controllers/adminRoutes.js
+++ controllers/adminRoutes.js
@@ -17,6 +17,39 @@ router.get('/', async (req, res) => {
   }
 });
 
+// admin login check 
+router.post('/login', async (req, res) => {
+  
+  // try {
+  //   // check the database for user that matches email 
+  //   const adminData = await admin.findOne({
+  //     where:
+  //       { email: req.body.email }
+  //   });
+
+  //   // check database to see if passwords match
+  //   const isValidPassword = await adminData.checkPassword(req.body.password);
+
+  //   if (!isValidPassword) {
+  //     res.status(400).json({ message: 'Wrong Email or Password' });
+  //     return;
+  //   }
+
+  //   // Admin's Session 
+  //   req.session.save(() => {
+  //     req.session.user_id = adminData.id;
+  //     req.session.logged_in = true;
+      
+  //     res.json({ user: adminData, message: 'logged in!' });
+  //   });
+  // } catch (err) {
+
+  //   // catching server errors 
+  //   res.status(500).json({ message: "Server Error", error: err })
+  // }
+
+})
+
 // admin dashboard 
 router.get('/dashboard', auth, async (req, res) => {
   try {
