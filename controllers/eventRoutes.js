a3e4954f901e59b978bc9b0b2ba28525b03c7776
aq032 <anthonyq032@gmail.com>
2022-08-26 17:45:41 -0700

    Fixed merge conflicts

--- controllers/eventRoutes.js
+++ controllers/eventRoutes.js
@@ -4,14 +4,9 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
-<<<<<<< HEAD
-    // rendering the homepage
-    res.render("events");
-=======
 
     // rendering the homepage 
     res.render('events')
->>>>>>> f5510809d8b742797eaf09a89224f6d55ac78d3f
   } catch (err) {
     // catching server errors
     res.status(500).json(err);
