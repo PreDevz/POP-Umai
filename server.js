b4fc71fede055be0740cc9ec75a0f207f0796846
Nate <itsnzte@gmail.com>
2022-08-27 12:06:37 -0700

    added force true in server js

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT} `));
 });
