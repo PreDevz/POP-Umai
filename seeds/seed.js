a0271ae0c0c3ba933048dd67802e191baae54235
Nate <itsnzte@gmail.com>
2022-08-27 17:20:21 -0700

    added adminSeed json file, and imported into seed js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,15 @@
 // Will seed events database
 const sequelize = require('../config/connection');
+
+// Import Models 
 const Event = require('../models/Event');
 const Admin = require('../models/Admin');
 
-const eventSeedData = require('./eventSeedData.json');
+// import Seeds 
+const eventSeed = require('./eventSeedData.json');
+const adminSeed = require('./adminData.json')
 
+// Add seeds data in database 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
