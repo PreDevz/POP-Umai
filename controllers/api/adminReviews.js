3483605c58ac9bee33a2ddc2740472ce9b6e031e
Nate <itsnzte@gmail.com>
2022-08-28 19:59:37 -0700

    removed admin json data in gitignore, and started the review routes for review routes branch

--- controllers/api/adminReviews.js
+++ controllers/api/adminReviews.js
@@ -6,13 +6,17 @@ const router = require('express').Router();
 // import Review Model 
 const Review = require('../../models/review');
 
-// Ex POST req:
-// {
-//   "name": "Nate's Event",
-//   "description": "Big event, you can't miss it",
-//   "event_date": "4/8/22",
-//   "event_time": "1am-11:59pm",
-//   "venue": "Churchill Ln",
-//   "location": "Perris, CA",
-//   "is_upcoming": true
-// }
+router.get('/:id', auth, async (req, res) => {
+
+  // Ex POST req:
+  // {
+  //   "name": "Nate's Event",
+  //   "description": "Big event, you can't miss it",
+  //   "event_date": "4/8/22",
+  //   "event_time": "1am-11:59pm",
+  //   "venue": "Churchill Ln",
+  //   "location": "Perris, CA",
+  //   "is_upcoming": true
+  // }
+
+})
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,17 @@
-// importing APIs 
-
+// Admin Dashboard Controller
+ 
 const router = require('express').Router();
 
+// importing APIs 
 const adminEvents = require('./adminEvents');
+const adminReviews = require('./adminReviews');
 const admin = require('./admin');
 
-// Admin Dashboard Controller
-// Events API 
+// Event APIs 
 router.use('/admin', admin);
 router.use('/admin-event', adminEvents);
+
+// Review APIs
+router.use('/admin-review', adminEvents);
 
 module.exports = router;
