b228c80514249702801aa180d389dc6f6fc4e42d
cesar-infante <cesar261110@gmail.com>
2022-08-29 12:24:26 -0700

    fixes to error message not showing when input field is null, if null will prompt to try again, else it will send the email if all input fields are complete

--- public/js/index.js
+++ public/js/index.js
@@ -6,10 +6,27 @@
 
 const btn = document.getElementById('contactSend');
 
+
+/* This is grabbing the forms input fields and making sure the value are not null, if they are null they will get an error message 
+to try again. if the fields arent null, the emailjs will send the form with serviceID, templateID, and the forms content,
+it will then confirm if the message was sent or if there was an error in the process.*/
 window.onload = () => {
     document.getElementById('form')
         .addEventListener('submit', function (event) {
             event.preventDefault();
+            const question = document.getElementById('option').value
+            // console.log(question)
+            const from_name = document.getElementById('from_name').value
+            // console.log(from_name)
+            const to_name = document.getElementById('contactEmail').value
+            // console.log(to_name)
+            const message = document.getElementById('message').value
+            // console.log(message)
+
+            if (question.value == "" || from_name.value == "" || to_name.value == "" || this.message.value == "") {
+                inputEmpty()
+                return
+            }
 
             btn.value = 'Sending...';
 
@@ -24,10 +41,20 @@ window.onload = () => {
                     btn.value = 'Send Email';
                     swal({
                         title: "Oops!",
-                        text: "You forgot a field :( please complete the whole form",
+                        text: "Something went wrong :(",
                         icon: "error",
                         button: "Try Again"
                     })
                 });
         });
+}
+
+/* If the user doesn't fill out the form, they will get a sweet alert message telling them to try again */
+function inputEmpty() {
+    swal({
+        title: "Oops!",
+        text: "You forgot a field :( please complete the whole form.",
+        icon: "error",
+        button: "Try Again"
+    })
 }
