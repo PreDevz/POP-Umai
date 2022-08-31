bcfd33f0bbac3264f1188c4fe34f8cf092a6696c
Nate <itsnzte@gmail.com>
2022-08-31 09:17:03 -0700

    renamed IDs and cached the main elements in login js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,11 @@
 // Will be used for admin login function
+
+// Cache The DOM
+
+// Email 
+const emailInput = document.getElementById('loginEmail')
+// Password 
+const passwordInput = document.getElementById('loginPass')
+// Login Button 
+const loginBtn = document.getElementById('loginBtn')
+
