b905768014ef1513e0ff1dc879f599a8fea3e6d9
Nate <itsnzte@gmail.com>
2022-08-31 09:24:34 -0700

    init login function and fetching backend to compare

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,7 @@
 // Will be used for admin login function
 
+require("dotenv").config();
+
 // Cache The DOM
 
 // Email 
@@ -9,3 +11,21 @@ const passwordInput = document.getElementById('loginPass')
 // Login Button 
 const loginBtn = document.getElementById('loginBtn')
 
+async function login() {
+  await fetch("/api/admin/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: process.env.ADMIN_NAME,
+      email: emailInput,
+      password: passwordInput
+    })
+  })
+    .then(() => {
+      location.replace('/dashboard')
+    })
+    .catch(
+      (err) => {
+        err ? console.log(err) : console.log("good")
+      });
+}
